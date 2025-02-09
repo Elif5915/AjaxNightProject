a663d25ca9493003763ac3e5419202809ffa63aa
Elif Yıldırım <elifyildirim3@outlook.com>
2025-02-09 20:36:00 +0300

    Ders 43 son eklemeleri

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using AjaxNightProject.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AjaxContext>();
 
 var app = builder.Build();
 
